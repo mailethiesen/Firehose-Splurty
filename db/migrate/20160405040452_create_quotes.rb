2a4deadeb8f656c2f0d3b3f7b6ccb9afb9c97156
mailethiesen <mailethiesen>
2016-04-05 04:41:26 +0000

    added quote.saying and quote.author to index.html.erb

--- db/migrate/20160405040452_create_quotes.rb
+++ db/migrate/20160405040452_create_quotes.rb
@@ -1,7 +1,6 @@
 class CreateQuotes < ActiveRecord::Migration
   def change
     create_table :quotes do |t|
-      #added new columns to model
       t.string :saying
       t.string :author
       t.timestamps
